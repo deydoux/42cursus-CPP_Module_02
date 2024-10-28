9cf7ca4fbc6fd64fb9546f561070c951d39cbd16
Dorian Eydoux <deydoux@student.42lyon.fr>
2024-10-28 12:57:58 +0100

    Update Point condtructor and operator debug messages

--- ex03/Point.cpp
+++ ex03/Point.cpp
@@ -1,19 +1,19 @@
 #include "Point.hpp"
 
 Point::Point() {
-	std::cerr << "Default const Pointructor called" << std::endl;
+	std::cerr << "Default Point constructor called" << std::endl;
 }
 
 Point::Point(const float x, const float y): _x(x), _y(y) {
-	std::cerr << "(" << x << "," << y << ") const Pointructor called" << std::endl;
+	std::cerr << "(" << x << "," << y << ") Point constructor called" << std::endl;
 }
 
 Point::Point(const Point &other): _x(other._x), _y(other._y) {
-	std::cerr << "Copy const Pointructor called" << std::endl;
+	std::cerr << "Point copy constructor called" << std::endl;
 }
 
 Point &Point::operator=(const Point &) {
-	std::cerr << "Copy assignement Point operator called" << std::endl;
+	std::cerr << "Point copy assignement operator called" << std::endl;
 	return (*this);
 }
 
